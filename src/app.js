e437888885672f223653f06c28e8c208f9e8b0d9
alisha1502 <alishabanka@gmail.com>
2026-08-23 20:15:09 +0530

    Playing with multiple request handlers for the same route and checking errors

--- src/app.js
+++ src/app.js
@@ -9,28 +9,47 @@ const app = express();
 //     res.send('Hello from HomePage!')
 // })
 
-app.get('/', (req, res) => {
-  res.send('Hello from Dashboard my darling!');
+// app.get('/', (req, res) => {
+//   res.send('Hello from Dashboard my darling!');
+// });
+
+// app.get('/user', (req, res) => {
+//   res.send('Hello from User Page!');
+// });
+
+// app.get('/user/:userid/:username', (req, res) => {
+//     console.log(req.query); // Log the query parameters to the console
+//     console.log(req.params); // Log the route parameters to the console
+//   res.send('Hello from User Page!');
+// });
+
+
+// app.post('/user', (req, res) => {
+//   // Handle form submission logic here
+//   res.send('User submitted successfully!');
+// });
+
+// app.put('/user', (req, res) => {
+//   // Handle update logic here
+//   res.send('User updated successfully!');
+// });
+
+// app.delete('/user', (req, res) => {
+//   // Handle delete logic here
+//   res.send('User deleted successfully!');
+// });
+
+//Multiple request handlers for the same route
+app.get('/user', (req, res, next) => {
+  console.log('First handler');
+//   res.send('Hello from First handler!');
+  next(); // Call the next handler 
+},(req,res,next)=>{
+    console.log('Second handler');
+    res.send('Hello from Second handler!');
+    next(); // Call the next handler
 });
 
-app.get('/user', (req, res) => {
-  res.send('Hello from User Page!');
-});
-
-app.post('/user', (req, res) => {
-  // Handle form submission logic here
-  res.send('User submitted successfully!');
-});
-
-app.put('/user', (req, res) => {
-  // Handle update logic here
-  res.send('User updated successfully!');
-});
-
-app.delete('/user', (req, res) => {
-  // Handle delete logic here
-  res.send('User deleted successfully!');
-});
 
 
 app.listen(3000, () => {
