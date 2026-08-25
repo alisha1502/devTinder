2a172d3bd6efcfa4280713c5a2dbabeb58b52af8
alisha1502 <alishabanka@gmail.com>
2026-08-25 14:41:46 +0530

    Add safety of my mongobdb atlas credentials publically

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const dns = require('dns'); dns.setServers(['8.8.8.8', '8.8.4.4']); 
 const mongoose = require('mongoose');
-const dbURI = 'mongodb+srv://alishabanka_db_user:lSwT8aCEIzAEgsII@learningnode.o2r8lhh.mongodb.net/'
+const dbURI = process.env.MONGO_URI;
 const dbConnect = async () =>{
  await mongoose.connect(dbURI);
 }
